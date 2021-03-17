bbb6c8b5d656026ab2f3bbc6463d87eefcf93aa4
abusedmedia <hello@fabiofranchino.com>
2021-03-17 09:23:39 +0100

    exposed color and font name conversion maps

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import main from './main'
 import utils from './utils'
 import defaults from './defaults'
+import { colors, fonts } from './thememap'
 
 export default {
   parse: main,
   findIndex: utils.findIndex,
   findRange: utils.findRange,
-  setting: defaults
+  setting: defaults,
+  colorsMap: colors,
+  fontsMap: fonts
 }
